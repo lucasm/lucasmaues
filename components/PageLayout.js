d7078d965969b28e90384dc0f93d302f0b6541f2
Lucas Menezes <lucasm@lucasm.dev>
2022-02-18 12:43:47 -0300

    yellow :)

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -12,29 +12,22 @@ export default function PageLayout(props) {
 		<title>{props.title}</title>
 		<meta name="description" content={props.description}/>
 		<meta name="author" content="Lucas Menezes" />
-			  
-		<link rel="icon" href="/favicon.ico" sizes="any"/>
 
 		<link rel="preconnect" href="https://fonts.gstatic.com"/>
 		<link href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;700&display=swap" rel="stylesheet"/>
 
+		<link rel="icon" href="/favicon.ico" sizes="any"/>
+		<link rel="icon" href="/icon.svg" type="image/svg+xml"/>
+		<link rel="manifest" href="/manifest.json"/>
+
 		<meta name="theme-color" content="#111"/>
 		<meta name="mobile-web-app-capable" content="yes"/>
 		<meta name="application-name" content="Lucas Menezes"/>
-		<link rel="icon" sizes="192x192" type="image/png" href="images/icons/icon-192x192.png"/>
-		<link rel="manifest" href="manifest.json"/>
-
 		<meta name="apple-mobile-web-app-capable" content="yes"/>
 		<meta name="apple-mobile-web-app-status-bar-style" content="translucent"/>
 		<meta name="apple-mobile-web-app-title" content="Lucas Menezes"/>
-		<link rel="apple-touch-icon" sizes="180x180" href="images/icons/apple-touch-icon-180x180.png"/>
-		<link rel="apple-touch-icon" sizes="167x167" href="images/icons/apple-touch-icon-167x167.png"/>
-		<link rel="apple-touch-icon" sizes="152x152" href="images/icons/apple-touch-icon-152x152.png"/>
-		<link rel="apple-touch-icon" sizes="120x120" href="images/icons/apple-touch-icon-120x120.png"/>
-		<link rel="apple-touch-icon" sizes="76x76" href="images/icons/apple-touch-icon-76x76.png"/>
-		<link rel="apple-touch-icon" href="apple-touch-icon.png"/>
-		<link rel="mask-icon" href="images/icons/safari-pinned-tab.svg" color="#111"/>
-
+		<link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
+		<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#111"/>
 		<meta name="msapplication-tap-highlight" content="no"/>
 
 		<meta itemprop="name" content="Lucas Menezes"/>
