cb389acb413a595fcdc0c6c4ace28a9f22e256e2
Lucas Menezes <lucasm@lucasm.dev>
2022-04-22 20:54:38 -0300

    fix: copy, btn secondary

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,7 @@ export default function Home() {
                 </h1>
 
                 {/* <figure id="profile">
-                    
-                        <img src="https://pbs.twimg.com/profile_images/1517213451621486592/hOGCMw8m_400x400.jpg" alt="Lucas Menezes profile photo" />
+                    <img src="https://pbs.twimg.com/profile_images/1517213451621486592/hOGCMw8m_400x400.jpg" alt="Lucas Menezes profile photo" />
                 </figure> */}
                     {/* //avatars.githubusercontent.com/lucasm */}
 
@@ -32,6 +31,7 @@ export default function Home() {
                     </a>
                     <a href="https://patreon.com/lucasm" target="_blank" rel="noopener" className="button button-secondary">
                         Donate me
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 783.74 695.32"><path d="M0,347.17,345.6,695.32l70.6-68.16L184.86,395.87H783.74V299.46H183.18l233-231.3L345.6,0,0,346.14l.53.52Z"/></svg>
                     </a>
                 </div>
             </div>
@@ -61,13 +61,13 @@ export default function Home() {
                 
                 <div className="specialContainer">
 
-                    <h2>Refining web experiences</h2>
+                    <h2>Scaling web experiences</h2>
 
-                    <p>I've been helping businesses and organizations launch digital products, like web applications, sites and e-commerces, that's establish an refined online presence and solve their problems.</p>
+                    <p>As consultant I've been helping businesses and organizations launch digital products, like web applications, sites and e-commerces, that's establish an refined online presence.</p>
 
-                    <p>I develop reactive front-end user interfaces with strong focus on user experience (responsive design, accessibility, performance and progressive enhancement) and adjacent back-end with APIs integrations and databases.</p>
+                    <p>I develop reactive front-end user interfaces with strong focus on user experience (performance, responsive design, accessibility, security and progressive enhancement) and adjacent back-end with APIs integrations and databases. Software architecture patterns are a point that I value.</p>
                         
-                   <p>I believe my work can empower others, so why I'm also write <a href="https://dev.to/lucasm">programming articles</a> and maintain <a href="https://patreon.com/lucasm">open source projects</a> for/by the community.</p>
+                   <p>I believe my work can empower others, so I write <a href="https://dev.to/lucasm" target="_blank" rel="noopener">programming articles</a> and maintain <a href="https://patreon.com/lucasm" target="_blank" rel="noopener">open source projects</a> for/by the community.</p>
 
                 </div>
 
