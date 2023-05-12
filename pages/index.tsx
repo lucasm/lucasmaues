43499f89deeea72545cec066eb6465a35bb571c7
Lucas Menezes <lucasm@lucasm.dev>
2023-05-11 23:26:55 -0300

    fix: copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,13 +8,13 @@ export default function Home() {
   return (
     <PageLayout
       title="Lucas Menezes — Software Engineer"
-      description="I'm a Software Engineer from Brazil. Coding to help businesses and people grow.">
+      description="Hello, I'm a Software Engineer from Brazil. I code to companies and people to grow.">
       <section className="fullscreen">
         <div className="fullscreen-container">
           <div className="container home">
             <div>
               <h1>Hello, I'm a Software Engineer from Brazil</h1>
-              <p className="description">I code to help businesses and people grow.</p>
+              <p className="description">I code for companies and people to grow.</p>
 
               <Icons />
             </div>
@@ -54,31 +54,34 @@ export default function Home() {
           <h2 className="subtitle">About me</h2>
 
           <div className="specialContainer">
-            <h2>I code to grow people and businesses </h2>
+            <h2>I'm a growth coder</h2>
 
             <p>
-              Since 2015 I've been coding digital products. Web apps, websites, e-commerces, PWAs
-              and mobile apps that helped organizations of all sizes establish a refined online
-              presence and grow their business.
+              Since 2015 I've been coding digital products. Web apps, websites, e-commerce, PWAs and
+              mobile apps that have helped organizations of all sizes, establish a polished online
+              presence, achieve results and grow their businesses.
             </p>
 
             <p>
-              I specialize in front-end development of reactive, responsive and component-based UIs
-              with focus on UX, performance, accessibility, progressive enhancement, security, web
-              standards and data analytics. Adjacent backend with APIs and databases integrations,
+              I specialize in front-end development of reactive and component-based UIs with a
+              strong focus on UX, performance, accessibility, progressive enhancement, security, web
+              standards and data analytics. Adjacent back-end with API and database integrations,
               containers, cloud services and CI/CD.
             </p>
 
             <p>
-              I'm building{' '}
+              I'm the founder of{' '}
               <a href="https://findto.app" target="_blank" rel="noopener">
                 Findto - AI decentralized search
-              </a>
-              , creating content and contributing with open source programming docs to support devs
-              community and advocate for the open web.
+              </a>{' '}
+              because I believe on the power of open web. I also create tech content and contribute
+              with open source programming docs to support devs community.
             </p>
 
-            <p>Make things happen!</p>
+            <p>
+              I am a lifelong learner, born in the Amazon region of Brazil, who loves to travel and
+              live new experiences. I highly value communication, innovation and respect.
+            </p>
           </div>
         </div>
       </section>
@@ -87,7 +90,7 @@ export default function Home() {
         <div className="container contactContainer">
           <div>
             <h2>Let's talk</h2>
-            <p>Drop me a line for contact, or if you want to talk about your project.</p>
+            <p>Do you want to talk with me? Drop me a line:</p>
           </div>
           <div>
             <p>
