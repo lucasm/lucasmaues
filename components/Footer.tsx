47b5ce005272f8b6a1eac427190b611cf088b5bb
Lucas Menezes <lucasm@lucasm.dev>
2023-07-20 19:19:07 -0300

    fix: add footer link description

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -8,6 +8,7 @@ const Footer = () => (
   <footer>
     <div className="container">
       <Link href="/">
+        Homepage
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
           <path d="M16,0A16,16,0,1,0,32,16,16,16,0,0,0,16,0Zm6,25H16V13H10V7H22Z" />
         </svg>
