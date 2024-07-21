674ea3e33f137d06627e334f4d90ff1249c061c5
Lucas Menezes <lucasm@lucasm.dev>
2024-07-20 21:27:37 -0300

    Refactor: title

--- components/PageLayout.tsx
+++ components/PageLayout.tsx
@@ -13,7 +13,7 @@ type Props = {
 export default function PageLayout(props: Props) {
   const title =
     props.title === 'Lucas Menezes'
-      ? 'Lucas Menezes | Frontend & AI Developer'
+      ? 'Lucas Menezes | Senior Frontend Developer'
       : props.title + ' | Lucas Menezes'
   const image = props.image || 'https://lucasm.dev/share.png'
 
