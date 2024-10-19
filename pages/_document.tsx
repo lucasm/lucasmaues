16348bdb284fb16d89dc24e242916527d860854a
Lucas Menezes <lucasm@lucasm.dev>
2024-10-18 22:42:59 -0300

    feat: web fonts local hosted

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,13 +4,8 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400..600&display=swap"
-          rel="stylesheet"
-        />
-        <link
-          href="https://db.onlinewebfonts.com/c/819f612977ba7ba86c14b739b908c73f?family=Styrene+A+Regular+Regular"
           rel="stylesheet"
         />
         {/* <link href="https://fonts.cdnfonts.com/css/maison-neue" rel="stylesheet" /> */}
